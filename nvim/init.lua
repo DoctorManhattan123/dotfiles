e135bb62b0f2afac2971b861bdb5f9aa3149da26
Alexander Berkov <alexander.berkov@titanom.com>
2024-04-24 14:30:14 +0200

    feat: adjust dotfiles repo

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,21 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require "core"
 
-require('nyarthan.bootstrap')
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-require('nyarthan.util')
-require('nyarthan.options')
-require('nyarthan.keymaps').setup()
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
-require('lazy').setup('nyarthan.plugins', {
-  defaults = {
-    lazy = true,
-  },
-})
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
